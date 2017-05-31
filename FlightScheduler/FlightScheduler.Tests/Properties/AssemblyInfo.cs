8617b92012c0a53c3bb368139e153691b7d98c5c
adamcrane <craneadam16@gmail.com>
2017-05-31 17:53:01 -0400

    Initial Commit

---
+++ FlightScheduler/FlightScheduler.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlightScheduler.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlightScheduler.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37ca9f8e-1a39-4c3e-a81c-5a47f02234ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FlightScheduler/FlightScheduler.Tests/WhenSchedulingFlights.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlightScheduler.Tests
+{
+    [TestClass]
+    public class WhenSchedulingFlights
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
